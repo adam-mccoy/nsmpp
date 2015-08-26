c56ddefc95936d7ab4ceac43aae75d7c76d793e7
Adam McCoy <adam@forsedomani.com>
2015-08-26 21:50:28 +1000

    Add SmppSession.

---
+++ src/NSmpp/SmppSession.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace NSmpp
+{
+    internal class SmppSession
+    {
+        private readonly Stream _inputStream;
+        private readonly Stream _outputStream;
+
+        public SmppSession(Stream inputStream, Stream outputStream)
+        {
+            _inputStream = inputStream;
+            _outputStream = outputStream;
+        }
+    }
+}
