f29cb443dca096bbb82b96cf8d464089846feb3c
Adam McCoy <adam@forsedomani.com>
2015-08-26 21:49:34 +1000

    Add PduSender.

---
+++ src/NSmpp/PduSender.cs
@@ -0,0 +1,24 @@
+﻿using NSmpp.Pdu;
+using NSmpp.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NSmpp
+{
+    internal class PduSender
+    {
+        private readonly Stream _outputStream;
+
+        internal PduSender(Stream outputStream)
+        {
+            _outputStream = outputStream;
+        }
+
+        internal async Task SendAsync(PduBase pdu)
+        {
+            var serializer = PduSerializerFactory.Create(pdu.Command);
+            var bytes = serializer.Serialize(pdu);
+            await _outputStream.WriteAsync(bytes, 0, bytes.Length);
+        }
+    }
+}
