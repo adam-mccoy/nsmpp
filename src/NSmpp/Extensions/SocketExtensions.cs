1c712d0a7fea846531c9a8dc32489aae0d7e97f6
Adam McCoy <adam@forsedomani.com>
2016-07-14 16:29:14 +1000

    Add SmppClient type.

---
+++ src/NSmpp/Extensions/SocketExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace NSmpp.Extensions
+{
+    internal static class SocketExtensions
+    {
+        internal static Task ConnectAsync(this Socket socket, string host, int port)
+        {
+            return Task.Factory.FromAsync(socket.BeginConnect(host, port, null, null), socket.EndConnect);
+        }
+    }
+}
---
+++ src/NSmpp/SmppClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+using NSmpp.Extensions;
+using NSmpp.Pdu;
+
+namespace NSmpp
+{
+    public class SmppClient
+    {
+        private SmppSession _currentSession = null;
+
+        public async Task Connect(string host, int port)
+        {
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            await socket.ConnectAsync(host, port);
+
+            var stream = new NetworkStream(socket);
+            _currentSession = new SmppSession(stream, stream);
+        }
+
+        public Task Bind(BindType type, string systemId, string password)
+        {
+            return _currentSession.Bind(type, systemId, password);
+        }
+    }
+}
