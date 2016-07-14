512d3b1cdcf68932fec4d5e856337af40bed855b
Adam McCoy <adam@forsedomani.com>
2016-07-14 16:35:24 +1000

    Add simple test client project.

---
+++ src/TestClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NSmpp;
+using NSmpp.Pdu;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var client = new SmppClient();
+                client.Connect("localhost", 2775).Wait();
+                client.Bind(BindType.Transmitter, "smppclient1", "password").Wait();
+                Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ruh Roh!");
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
