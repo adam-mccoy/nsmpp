6e50bb68ab0d2b7b24dcc4a61a90f50039a53c58
Adam McCoy <adam@forsedomani.com>
2016-07-29 09:44:33 +1000

    Add test submissions.

--- src/TestClient/Program.cs
+++ src/TestClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using NSmpp;
 
 namespace TestClient
@@ -14,7 +16,13 @@ namespace TestClient
 
                     client.Connect("localhost", 2775).Wait();
                     client.Bind(BindType.Transmitter, "smppclient1", "password").Wait();
-                    Console.WriteLine("Bound. Press ENTER to quit.");
+                    Console.WriteLine("Session bound. Press ENTER to start sending.");
+                    Console.ReadKey();
+
+                    var tasks = Enumerable.Range(0, 100).Select(i => SendMessage(client, i));
+                    Task.WhenAll(tasks).Wait();
+
+                    Console.WriteLine("Done. Press ENTER to quit.");
                     Console.ReadKey(true);
                 }
             }
@@ -23,6 +31,15 @@ namespace TestClient
                 Console.WriteLine("Ruh Roh!");
                 Console.WriteLine(ex);
             }
+        }
+
+        public static async Task SendMessage(SmppClient client, int i)
+        {
+            var number = i.ToString("0000000000");
+            var message = $"This is test message #{i}";
+            Console.WriteLine($"Submitting message {i}...");
+            var result = await client.Submit(number, number, message);
+            Console.WriteLine($"Message {i} submitted with message ID {result.MessageId}.");
         }
     }
 }
