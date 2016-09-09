ed4d6f2606a33eb3ef1221b3afabd2b1328a9521
Adam McCoy <adam@forsedomani.com>
2016-09-09 14:49:11 +1000

    Add query response PDU serializer stub and tests.

---
+++ src/NSmpp/Serialization/QueryResponseSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NSmpp.Pdu;
+
+namespace NSmpp.Serialization
+{
+    internal class QueryResponseSerializer : PduSerializer<QueryResponse>
+    {
+        internal override QueryResponse Deserialize(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override byte[] Serialize(QueryResponse pdu)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
