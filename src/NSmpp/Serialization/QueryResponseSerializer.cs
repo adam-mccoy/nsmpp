c6dc31d53ffd5bf9cfe370bc90bac888d5809869
Adam McCoy <adam@forsedomani.com>
2016-09-12 20:24:39 +1000

    Implement query response PDU serializer.

--- src/NSmpp/Serialization/QueryResponseSerializer.cs
+++ src/NSmpp/Serialization/QueryResponseSerializer.cs
@@ -1,18 +1,41 @@
-﻿using System;
-using NSmpp.Pdu;
+﻿using NSmpp.Pdu;
 
 namespace NSmpp.Serialization
 {
     internal class QueryResponseSerializer : PduSerializer<QueryResponse>
     {
-        internal override QueryResponse Deserialize(byte[] bytes)
-        {
-            throw new NotImplementedException();
-        }
-
         internal override byte[] Serialize(QueryResponse pdu)
         {
-            throw new NotImplementedException();
+            var writer = new PduWriter();
+            writer.WritePduHeader(pdu);
+            writer.WriteString(pdu.MessageId);
+            writer.WriteByte(0x00); // write date
+            writer.WriteByte((byte)pdu.MessageState);
+            writer.WriteByte((byte)pdu.ErrorCode);
+
+            return Finalize(writer);
+        }
+
+        internal override QueryResponse Deserialize(byte[] bytes)
+        {
+            var reader = new PduReader(bytes);
+            var length = reader.ReadInteger();
+            var command = (SmppCommand)reader.ReadInteger();
+            var status = (SmppStatus)reader.ReadInteger();
+            var sequence = (uint)reader.ReadInteger();
+            var messageId = reader.ReadString();
+            // read date
+            var finalDate = reader.ReadString();
+            var messageState = (MessageState)reader.ReadByte();
+            var errorCode = reader.ReadByte();
+
+            return new QueryResponse(
+                status,
+                sequence,
+                messageId,
+                null,
+                messageState,
+                errorCode);
         }
     }
 }
