cd202901439055e4e9912c874eae0111076ca7e9
Adam McCoy <adam@forsedomani.com>
2016-10-25 08:07:46 +1100

    Include additional status codes.

--- src/NSmpp/Pdu/SmppStatus.cs
+++ src/NSmpp/Pdu/SmppStatus.cs
@@ -2,7 +2,10 @@
 {
     internal enum SmppStatus
     {
-        Ok               = 0x00000000,
-        MessageQueueFull = 0x00000014
+        Ok                   = 0x00000000,
+        InvalidMessageLength = 0x00000001,
+        InvalidCommandLength = 0x00000002,
+        InvalidCommandId     = 0x00000003,
+        MessageQueueFull     = 0x00000014
     }
 }
