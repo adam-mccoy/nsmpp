d066461b89e4307288bedab07a1e21248f6f6376
Adam McCoy <adam@forsedomani.com>
2016-10-25 08:10:27 +1100

    Add tests for generic nack serializer.

---
+++ src/NSmpp/Serialization/GenericNackSerializer.cs
@@ -0,0 +1,17 @@
+﻿using NSmpp.Pdu;
+
+namespace NSmpp.Serialization
+{
+    internal class GenericNackSerializer : PduSerializer<GenericNack>
+    {
+        internal override byte[] Serialize(GenericNack pdu)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override GenericNack Deserialize(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/NSmpp.Tests/GenericNackSerializerTests.cs
@@ -0,0 +1,52 @@
+﻿using NSmpp.Pdu;
+using NSmpp.Serialization;
+using NUnit.Framework;
+
+namespace NSmpp.Tests
+{
+    [TestFixture]
+    public class GenericNackSerializerTests
+    {
+        [TestCase(0x01)]
+        [TestCase(0x02)]
+        [TestCase(0x03)]
+        public void Deserializes_Pdu(byte statusByte)
+        {
+            var data = new byte[]
+            {
+                0x00, 0x00, 0x00, 0x10, // length
+                0x80, 0x00, 0x00, 0x00, // command
+                0x00, 0x00, 0x00, statusByte, // status
+                0x00, 0x00, 0x00, 0x20, // sequence
+            };
+
+            var serializer = new GenericNackSerializer();
+            var pdu = serializer.Deserialize(data);
+
+            Assert.AreEqual(SmppCommand.GenericNack, pdu.Command);
+            Assert.AreEqual(32, pdu.SequenceNumber);
+            Assert.AreEqual((SmppStatus)statusByte, pdu.Status);
+        }
+
+        [TestCase(0x01)]
+        [TestCase(0x02)]
+        [TestCase(0x03)]
+        public void Serializes_Pdu(byte statusByte)
+        {
+            var expectedResult = new byte[]
+            {
+                0x00, 0x00, 0x00, 0x10, // length
+                0x80, 0x00, 0x00, 0x00, // command
+                0x00, 0x00, 0x00, statusByte, // status
+                0x00, 0x00, 0x00, 0x20, // sequence
+            };
+
+            var pdu = new GenericNack((SmppStatus)statusByte, 32);
+            var serializer = new GenericNackSerializer();
+
+            var result = serializer.Serialize(pdu);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+    }
+}
