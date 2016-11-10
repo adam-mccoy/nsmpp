a2d2fbabcc1d56032013f705411279707c0b42de
Adam McCoy <adam@forsedomani.com>
2016-11-10 16:22:08 +1100

    Add tests for cancel serializer.

---
+++ src/NSmpp/Serialization/CancelSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NSmpp.Pdu;
+
+namespace NSmpp.Serialization
+{
+    internal class CancelSerializer : PduSerializer<Cancel>
+    {
+        internal override Cancel Deserialize(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override byte[] Serialize(Cancel pdu)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
