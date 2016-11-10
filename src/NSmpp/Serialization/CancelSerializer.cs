aafc894aae33465b4dc2e9378038b5a5bd4c2a73
Adam McCoy <adam@forsedomani.com>
2016-11-11 09:52:06 +1100

    Implement cancel serializer.

--- src/NSmpp/Serialization/CancelSerializer.cs
+++ src/NSmpp/Serialization/CancelSerializer.cs
@@ -5,14 +5,28 @@ namespace NSmpp.Serialization
 {
     internal class CancelSerializer : PduSerializer<Cancel>
     {
-        internal override Cancel Deserialize(byte[] bytes)
-        {
-            throw new NotImplementedException();
-        }
-
         internal override byte[] Serialize(Cancel pdu)
         {
-            throw new NotImplementedException();
+            var writer = new PduWriter();
+            writer.WritePduHeader(pdu);
+            writer.WriteString(pdu.ServiceType);
+            writer.WriteString(pdu.MessageId);
+            writer.WriteAddress(pdu.Source);
+            writer.WriteAddress(pdu.Destination);
+            return Finalize(writer);
+        }
+
+        internal override Cancel Deserialize(byte[] bytes)
+        {
+            var reader = new PduReader(bytes);
+            reader.Skip(12);
+            var sequence = (uint)reader.ReadInteger();
+            var serviceType = reader.ReadString();
+            var messageId = reader.ReadString();
+            var source = reader.ReadAddress();
+            var dest = reader.ReadAddress();
+
+            return new Cancel(sequence, serviceType, messageId, source, dest);
         }
     }
 }
