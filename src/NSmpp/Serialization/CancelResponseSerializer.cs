77f6e89f17ae1b8dfb464c8400b7119a676fefb4
Adam McCoy <adam@forsedomani.com>
2016-12-12 09:55:54 +1100

    Add cancel response PDU serializer and tests.

---
+++ src/NSmpp/Serialization/CancelResponseSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NSmpp.Pdu;
+
+namespace NSmpp.Serialization
+{
+    internal class CancelResponseSerializer : PduSerializer<CancelResponse>
+    {
+        internal override byte[] Serialize(CancelResponse pdu)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override CancelResponse Deserialize(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
