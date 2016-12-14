5f0e12e29e89f65b9276d7d7ead42dfc2547167c
Adam McCoy <adam@forsedomani.com>
2016-12-14 11:01:40 +1100

    Add deliver serializer and tests.

---
+++ src/NSmpp/Serialization/DeliverSerializer.cs
@@ -0,0 +1,18 @@
+﻿using NSmpp.Helpers;
+using NSmpp.Pdu;
+
+namespace NSmpp.Serialization
+{
+    internal class DeliverSerializer : PduSerializer<Deliver>
+    {
+        internal override byte[] Serialize(Deliver pdu)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override Deliver Deserialize(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
