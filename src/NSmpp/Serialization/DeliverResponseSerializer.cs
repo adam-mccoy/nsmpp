168c6e9126870e90705fed522899de496a211d47
Adam McCoy <adam@forsedomani.com>
2016-12-14 11:14:10 +1100

    Add deliver response serializer and tests.

---
+++ src/NSmpp/Serialization/DeliverResponseSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NSmpp.Pdu;
+
+namespace NSmpp.Serialization
+{
+    internal class DeliverResponseSerializer : PduSerializer<DeliverResponse>
+    {
+        internal override byte[] Serialize(DeliverResponse pdu)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override DeliverResponse Deserialize(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/NSmpp.Tests/DeliverResponseSerializerTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using NSmpp.Pdu;
+using NSmpp.Serialization;
+
+namespace NSmpp.Tests
+{
+    [TestFixture]
+    public class DeliverResponseSerializerTests
+    {
+        #region Test Data
+        private static readonly byte[] EncodedPdu = new byte[]
+        {
+            0x00, 0x00, 0x00, 0x11, // length
+            0x80, 0x00, 0x00, 0x05, // command
+            0x00, 0x00, 0x00, 0x00, // status
+            0x00, 0x00, 0x00, 0x10, // sequence
+            0x00                    // message id
+        };
+        #endregion
+
+        [Test]
+        public void Serializes_Pdu()
+        {
+            var expectedResult = EncodedPdu;
+
+            var pdu = new DeliverResponse(SmppStatus.Ok, 16);
+            var serializer = new DeliverResponseSerializer();
+            var result = serializer.Serialize(pdu);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Deserializes_Pdu()
+        {
+            var serializer = new DeliverResponseSerializer();
+            var pdu = serializer.Deserialize(EncodedPdu);
+
+            Assert.AreEqual(16, pdu.SequenceNumber);
+            Assert.AreEqual(SmppStatus.Ok, pdu.Status);
+        }
+    }
+}
