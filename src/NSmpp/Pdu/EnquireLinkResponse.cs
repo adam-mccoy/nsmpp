c1f708f4597822358fa00668835878e8a0172a6a
Adam McCoy <adam@forsedomani.com>
2017-01-17 09:32:11 +1100

    Add enquire link response PDU.

---
+++ src/NSmpp/Pdu/EnquireLinkResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace NSmpp.Pdu
+{
+    internal class EnquireLinkResponse : ResponsePduBase
+    {
+        public EnquireLinkResponse(SmppStatus status, uint sequenceNumber)
+            : base(status, sequenceNumber)
+        {
+        }
+
+        internal override SmppCommand Command
+        {
+            get { return SmppCommand.EnquireLinkResp; }
+        }
+    }
+}
