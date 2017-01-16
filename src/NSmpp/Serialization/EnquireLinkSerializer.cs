18e2b9e75d76d1fcfc71cbddef0793937c7fda53
Adam McCoy <adam@forsedomani.com>
2017-01-17 09:30:20 +1100

    Add enquire link serializer and tests.

---
+++ src/NSmpp/Serialization/EnquireLinkSerializer.cs
@@ -0,0 +1,17 @@
+﻿using NSmpp.Pdu;
+
+namespace NSmpp.Serialization
+{
+    internal class EnquireLinkSerializer : PduSerializer<EnquireLink>
+    {
+        internal override byte[] Serialize(EnquireLink pdu)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override EnquireLink Deserialize(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
