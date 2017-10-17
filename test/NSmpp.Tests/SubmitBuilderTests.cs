8a1faf1d0d929f62c2bcbae0ae641d54612bc85d
Adam McCoy <adam@forsedomani.com>
2017-10-17 11:51:42 +1100

    Add tests for setting ESM class values in submit builder.

---
+++ test/NSmpp.Tests/SubmitBuilderTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace NSmpp.Tests
+{
+    [TestFixture]
+    public class SubmitBuilderTests
+    {
+        [Test]
+        public void Sets_Messaging_Mode_Idempotently()
+        {
+            var builder = new SubmitBuilder();
+
+            builder.UseMessagingMode(MessagingMode.Datagram);
+            Assert.AreEqual(0x01, builder.Build().EsmClass);
+            builder.UseMessagingMode(MessagingMode.Forward);
+            Assert.AreEqual(0x02, builder.Build().EsmClass);
+            builder.UseMessagingMode(MessagingMode.Default);
+            Assert.AreEqual(0x00, builder.Build().EsmClass);
+        }
+
+        [Test]
+        public void Sets_Message_Type_Idempotently()
+        {
+            var builder = new SubmitBuilder();
+
+            builder.UseMessageType(MessageType.DeliveryAcknowledgement);
+            Assert.AreEqual(0x08, builder.Build().EsmClass);
+            builder.UseMessageType(MessageType.ManualUserAcknowledgement);
+            Assert.AreEqual(0x10, builder.Build().EsmClass);
+            builder.UseMessageType(MessageType.Default);
+            Assert.AreEqual(0x00, builder.Build().EsmClass);
+        }
+
+        [Test]
+        public void Sets_Network_Specific_Features_Idempotently()
+        {
+            var builder = new SubmitBuilder();
+
+            builder.UseGsmFeatures(NetworkSpecificFeatures.SetReplyPath);
+            Assert.AreEqual(0x80, builder.Build().EsmClass);
+            builder.UseGsmFeatures(NetworkSpecificFeatures.UdhiIndicator);
+            Assert.AreEqual(0x40, builder.Build().EsmClass);
+            builder.UseGsmFeatures(NetworkSpecificFeatures.SetReplyPath | NetworkSpecificFeatures.UdhiIndicator);
+            Assert.AreEqual(0xc0, builder.Build().EsmClass);
+            builder.UseGsmFeatures(NetworkSpecificFeatures.None);
+            Assert.AreEqual(0x00, builder.Build().EsmClass);
+        }
+    }
+}
